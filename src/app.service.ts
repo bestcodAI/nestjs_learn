74015ef5749fb7ed1db25afbf25b4dcfe467a6c6
Chou Chamnan <109691277+bestcodAI@users.noreply.github.com>
2024-08-03 19:59:31 +0700

    Three commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! learn with me';
+    return 'Hello World! learn with me (Chou Chamnan)';
   }
 }
